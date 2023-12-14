831b5eb3c8c6632a4df4d317866a01fcb1ddc896
Adon Ahmed Joy <133283872+Adon159@users.noreply.github.com>
2023-12-14 21:00:07 +0600

    Update Leetcode 1.py

--- Leetcode 1.py
+++ Leetcode 1.py
@@ -1,24 +1,23 @@
-word1 = input("Enter a word: ")
-word2 = input("Enter another word: ")
-word3 = ""
-if len(word1) == len(word2):
-    for chr in range(0, len(word1)):
-        word3 += word1[chr]
-        word3 += word2[chr]
-    print(word3)
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        word3 = ""
+        if len(word1) == len(word2):
+            for chr in range(0, len(word1)):
+                word3 += word1[chr]
+                word3 += word2[chr]
+            return word3
 
-elif len(word1) > len(word2):
-    for chr in range(0, len(word2)):
-        word3 += word1[chr]
-        word3 += word2[chr]
-    word3 += word1[len(word2):]
-    print(word3)
+        elif len(word1) > len(word2):
+            for chr in range(0, len(word2)):
+                word3 += word1[chr]
+                word3 += word2[chr]
+            word3 += word1[len(word2):]
+            return word3
 
-elif len(word2) > len(word1):
-    for chr in range(0, len(word1)):
-        word3 += word1[chr]
-        word3 += word2[chr]
-    word3 += word2[len(word1):]
-    print(word3)
-
+        elif len(word2) > len(word1):
+            for chr in range(0, len(word1)):
+                word3 += word1[chr]
+                word3 += word2[chr]
+            word3 += word2[len(word1):]
+            return word3
 
